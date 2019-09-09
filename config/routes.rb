167b2bcfb5260d346bb022ee8664f6b0080f845e
Olya <solyaserkova@gmail.com>
2019-09-09 11:59:05 +0300

    project and task added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/new'
+
+  get 'projects/edit'
+
+  get 'projects/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
