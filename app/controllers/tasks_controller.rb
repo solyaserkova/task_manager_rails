5fafef6642149ece1e28480c41095d614466a016
Olya <solyaserkova@gmail.com>
2019-09-09 20:13:51 +0300

    able to delete and edit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,34 @@
 class TasksController < ApplicationController
+  before_action :set_project
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = @project.tasks
+  end
+
   def create
     @project = Project.find(params[:project_id])
     @task = @project.tasks.create(task_params)
     redirect_to project_path(@project)
   end
 
-  def destroy
-    @project = Project.find(params[:project_id])
+  def edit
+    # @project = Project.find(params[:project_id])
     @task = @project.tasks.find(params[:id])
+  end
+
+  def update
+    @task = @project.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @project = Project.find(params[:project_id])
+    # @task = @project.tasks.find(params[:id])
     @task.destroy
     redirect_to project_path(@project)
   end
@@ -16,4 +37,13 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:name, :status)
     end
+
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    def set_task
+      @task = @project.tasks.find(params[:id])
+    end
+
 end
